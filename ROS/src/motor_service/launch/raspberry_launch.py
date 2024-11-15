af0701d91cf481df3d3728b328de2a5b1e74623b
kasper georen <kasper.georen@gmail.com>
2024-11-15 16:42:14 +0100

    Added raspberry launch

---
+++ ROS/src/motor_service/launch/raspberry_launch.py
@@ -0,0 +1,20 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+def generate_launch_description():
+   return LaunchDescription([
+
+    Node(
+       package="motor_service",
+       executable="motor_controller_service",
+       name='motor_controller',
+       parameters=[
+                {'baud_rate': 115200},
+                {'handshake_response_byte': 0xA6},
+                {'timeout': 2.0}, #handshake timeout
+                {'publish_rate': 20.0},  # Set your desired publish rate here
+                {'right_motor_topic': 'right_motor_rpm'},  # Custom topic for right motor
+                {'left_motor_topic': 'left_motor_rpm'},  # Custom topic for left motor
+                {'encoder_angle_topic': 'encoder_angle_deg'},  # Custom topic for encoder angle
+            ]
+    )
+   ])

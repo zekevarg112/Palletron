d654e8d32b749c4f258a8d1e4b699b4526c9ecd4
viktorwikstrom <viktor.wikstrom@hotmail.com>
2024-11-18 10:19:17 +0100

    Added linak node to Pi launch file

--- ROS/src/motor_service/launch/raspberry_launch.py
+++ ROS/src/motor_service/launch/raspberry_launch.py
@@ -11,10 +11,17 @@ def generate_launch_description():
                 {'baud_rate': 115200},
                 {'handshake_response_byte': 0xA6},
                 {'timeout': 2.0}, #handshake timeout
-                {'publish_rate': 20.0},  # Set your desired publish rate here
+                {'publish_rate': 40.0},  # Set your desired publish rate here
                 {'right_motor_topic': 'right_motor_rpm'},  # Custom topic for right motor
                 {'left_motor_topic': 'left_motor_rpm'},  # Custom topic for left motor
                 {'encoder_angle_topic': 'encoder_angle_deg'},  # Custom topic for encoder angle
             ]
+    ),
+    Node(
+        package='articubot_one',
+        executable='linear_actuator_controller_irl_node.py',
+        name='linear_actuator_controller_irl_node',
+        output='screen',
+        parameters=[{'use_sim_time': False}]
     )
    ])
